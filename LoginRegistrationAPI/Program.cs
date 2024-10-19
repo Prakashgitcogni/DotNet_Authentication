6d49faa5e63840575348e98133c98d475b48af9a
Prakash Lulla <prakashgitcogni@gmail.com>
2024-10-19 11:22:54 +0530

    implemented CORS

--- LoginRegistrationAPI/Program.cs
+++ LoginRegistrationAPI/Program.cs
@@ -17,6 +17,16 @@ builder.Services.AddSwaggerGen();
 //builder.Services.AddDbContext<TestDBContext>(options =>
 // options.UseSqlServer(builder.Configuration.GetConnectionString("dbcs"))
 //);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 builder.Services.AddDbContext<AUTHDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("dbcsAuth"))
 );
